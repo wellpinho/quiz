4d003102b764f17c99156f64fdc2fc0ef5393991
wellpinho <wellpinho@outlook.com>
2023-05-28 11:50:37 -0300

    init: project initial

---
+++ app.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('.quiz-form');
+const showResult = document.querySelector('.modal-body');
+const corretAnswers = ['B', 'B', 'B', 'B'];
+
+const response = {
+    25: 'Que pena, sua nota esta abaixo da média.',
+    50: 'Sua nota esta na média precisa melhorar.',
+    75: 'Muito bom sua nota esta entre os 75% que acertaram acima de 75',
+    100: 'Wow, sua not aficou nos top 100. Parabéns!'
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    let score = 0;
+    const userAnswers = [
+        form.inputQuestion1.value,
+        form.inputQuestion2.value,
+        form.inputQuestion3.value,
+        form.inputQuestion4.value,
+    ];
+
+    userAnswers.forEach((userAnswer, index) => {
+        if (userAnswer === corretAnswers[index]) {
+            score += 25;
+        }
+    })
+    
+
+    showResult.textContent = response[score];
+})
+
